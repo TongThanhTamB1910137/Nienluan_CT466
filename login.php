f26e62fad121e6478aad6b2144889115e46aaa5e
TongThanhTam_B1910137 <tamB1910137@student.ctu.edu.vn>
2023-02-27 15:15:07 +0700

    ket noi den server, tao va dieu chinh trang, dang nhap, dang xuat

---
+++ login.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/login.css">
+
+	  <style type="text/css">
+	  #buttn{
+		  color:#fff;
+		  background-color: #ff3300;
+	  }
+	  </style>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  
+</head>
+<body>
+<!--header starts-->
+<header id="header" class="header-scroll top-header headrom">
+            <!-- .navbar -->
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Sign Up</a> </li>';
+							}
+						else
+							{
+									//if user is login
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+            <!-- /.navbar -->
+        </header>
+		<div style=" background-image: url('images/img/pimg.jpg');">
+
+<body>
+<?php
+include("connection/connect.php"); //INCLUDE CONNECTION
+error_reporting(0); // hide undefine index errors
+session_start(); // temp sessions
+if(isset($_POST['submit']))   // if button is submit
+{
+	$username = $_POST['username'];  //fetch records from login form
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"]))   // if records were not empty
+     {
+	$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
+	$result=mysqli_query($db, $loginquery); //executing
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row))  // if matching records in the array & if everything is right
+								{
+                                    	$_SESSION["user_id"] = $row['u_id']; // put user id into temp session
+										 header("refresh:1;url=index.php"); // redirect to index.php page
+	                            } 
+							else
+							    {
+                                      	$message = "Invalid Username or Password!"; // throw error
+                                }
+	 }
+	
+	
+}
+?>
+  
+<!-- Form Mixin-->
+<!-- Input Mixin-->
+<!-- Button Mixin-->
+<!-- Pen Title-->
+<div class="pen-title">
+  <!-- <h1>Login Page</h1> -->
+</div>
+<!-- Form Module-->
+<div class="module form-module">
+  <div class="toggle">
+   
+  </div>
+  <div class="form">
+    <h2>Login to your account</h2>
+	  <span style="color:red;"><?php echo $message; ?></span> 
+   <span style="color:green;"><?php echo $success; ?></span>
+    <form action="" method="post">
+      <input type="text" placeholder="Username"  name="username"/>
+      <input type="password" placeholder="Password" name="password"/>
+      <input type="submit" id="buttn" name="submit" value="Login" />
+    </form>
+  </div>
+  
+  <div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <section class="app-section">
+            <div class="app-wrap">
+                <div class="container">
+                    <div class="row text-img-block text-xs-left">
+                        <div class="container">
+                            <div class="col-xs-12 col-sm-5 right-image text-center">
+                                <figure> <img src="images/app.png" alt="Right Image" class="img-fluid"> </figure>
+                            </div>
+                            <div class="col-xs-12 col-sm-7 left-text">
+                                <h3>Food Picky - The Best Food Delivery App</h3>
+                                <p>Got Hungry? Get the food you want, from the restaurants you love, delivered at blinking speed.
+
+								Eat what you like, where you like, when you like. Find the local flavours you crave, all at the tap of a button.</p>
+                                <div class="social-btns">
+                                    <a href="#" class="app-btn apple-button clearfix">
+                                        <div class="pull-left"><i class="fa fa-apple"></i> </div>
+                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
+                                    </a>
+                                    <a href="#" class="app-btn android-button clearfix">
+                                        <div class="pull-left"><i class="fa fa-android"></i> </div>
+                                        <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play Store</span> </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- start: FOOTER -->
+        <footer class="footer">
+            <div class="container">
+                <!-- top footer statrs -->
+                <div class="row top-footer">
+                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Choose it &amp; Enjoy your meals! </span> </div>
+                    <div class="col-xs-12 col-sm-2 about color-gray">
+                        <h5>About Us</h5>
+                        <ul>
+                            <li><a href="#">Our Mission</a></li>
+                            <li><a href="#">Social Media</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                            <li><a href="#">We are hiring</a></li>
+							<li><a href="#">Join us Today</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>How it Works?</h5>
+                        <ul>
+                            <li><a href="#">Enter your location</a></li>
+                            <li><a href="#">Choose the restaurant</a></li>
+                            <li><a href="#">Choose your dishes</a></li>
+                            <li><a href="#">Get delivered</a></li>
+							<li><a href="#">Pay on delivery</a></li>
+							<li><a href="#">Enjoy your meals :)</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 pages color-gray">
+                        <h5>Legal</h5>
+                        <ul>
+                            <li><a href="#">Terms & Conditions</a> </li>
+                            <li><a href="#">Refund & Cancellation</a> </li>
+                            <li><a href="#">Privacy Policy</a> </li>
+                            <li><a href="#">Cookie Policy</a> </li>
+                            <li><a href="#">Offer Terms</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                        <h5>Locations We Deliver To</h5>
+                        <ul>
+                            <li><a href="#">Chennai</a> </li>
+                            <li><a href="#">Kanchipuram</a> </li>
+                            <li><a href="#">Tiruchy</a> </li>
+                            <li><a href="#">Salem</a> </li>
+                            <li><a href="#">Madurai</a> </li>
+                            <li><a href="#">Theni</a> </li>
+                            <li><a href="#">Thiruvallur</a> </li>
+                            <li><a href="#">Pondicherry</a> </li>
+                            <li><a href="#">Thoothukudi</a> </li>
+                            <li><a href="#">Kanyakumari</a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- top footer ends -->
+                <!-- bottom footer statrs -->
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>All Major Credit Cards Accepted</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Address:</h5>
+                            <p>Mahatma Gandhi Salai, Chennai - 600034.</p>
+                            <h5>Call us at: <a href="tel:+914450005500">+91 44 50005500</a></h5></div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>Who are we?</h5>
+                            <p>Launched in 2021, Our technology platform connects customers, restaurant partners and delivery partners, serving their multiple needs. Customers use our platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book a table and make payments while dining-out at restaurants.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- bottom footer ends -->
+            </div>
+        </footer>
+        <!-- end:Footer -->
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+
+
+</body>
+
+</html>
